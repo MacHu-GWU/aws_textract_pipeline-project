8ac05047234b0b988d01925141611e7510aa9512
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-04-17 16:59:30 -0400

    add doc type

--- aws_textract_pipeline/api.py
+++ aws_textract_pipeline/api.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 
+"""
+Usage example::
+
+    import aws_textract_pipeline.api as aws_textract_pipeline
+"""
+
+from .doc_type import DocTypeEnum
+from .doc_type import S3ContentTypeEnum
